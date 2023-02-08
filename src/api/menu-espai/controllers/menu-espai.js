b71770fb9a8090a6d9b56935833da4e57c5150e1
jordisanchezntq <jordi.sanchez@gfk.com>
2023-02-08 15:19:29 +0100

    updating 08.02.2023

---
+++ src/api/menu-espai/controllers/menu-espai.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * menu-espai controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::menu-espai.menu-espai');
---
+++ src/api/menu-espai/routes/menu-espai.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * menu-espai router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::menu-espai.menu-espai');
