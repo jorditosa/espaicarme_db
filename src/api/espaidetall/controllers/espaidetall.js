3b196bfd71ba56575c564fa7aa950146ed5fbd56
jorditosa <jordisato88@gmail.com>
2024-04-06 09:39:13 +0200

    permissions in espai details

--- src/api/espaidetall/controllers/espaidetall.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * espaidetall controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::espaidetall.espaidetall');
--- src/api/espaidetall/routes/espaidetall.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * espaidetall router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::espaidetall.espaidetall');
