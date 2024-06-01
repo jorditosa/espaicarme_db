e146714135529ddec048c096634100922e476825
js <js@js>
2024-06-01 08:57:48 +0200

    changed vars

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cffmj6pa6gdma8kkcc50-a.frankfurt-postgres.render.com'),
+      host: env('DATABASE_HOST', 'dpg-cpdc8tdds78s73ec27vg-a.frankfurt-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'espaicarme_mjky'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'ikcBAshL7YuWjxF7v144W1inrGxHG8bR'),
+      database: env('DATABASE_NAME', 'espaicarme_web'),
+      user: env('DATABASE_USERNAME', 'espaicarme_web_user'),
+      password: env('DATABASE_PASSWORD', 'd7tV7w5CHQOAueKTfkh09Z98bpH8Amgv'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
