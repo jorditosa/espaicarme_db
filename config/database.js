56cd86e57944562c10ed00e33d02307a33a0ca9a
js <js@js>
2024-06-01 09:11:27 +0200

    old vars

--- config/database.js
+++ config/database.js
@@ -2,6 +2,36 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
+      host: env('DATABASE_HOST', 'dpg-cffmj6pa6gdma8kkcc50-a.frankfurt-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'espaicarme_mjky'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'ikcBAshL7YuWjxF7v144W1inrGxHG8bR'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+  },
+});
+
+/*
+OLD CREDENTIALS
+
+root
+
+db_name: espaicarme_mjky
+
+
+postgres://root:ikcBAshL7YuWjxF7v144W1inrGxHG8bR@dpg-cffmj6pa6gdma8kkcc50-a.frankfurt-postgres.render.com/espaicarme_mjky
+
+
+db_passwd: ikcBAshL7YuWjxF7v144W1inrGxHG8bR
+
+
+
+
+NEW CREDENTIALS 
+
+    client: 'postgres',
+    connection: {
       host: env('DATABASE_HOST', 'dpg-cpdc8tdds78s73ec27vg-a.frankfurt-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'espaicarme_web'),
@@ -11,3 +41,5 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+*/
