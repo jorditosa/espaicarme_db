e25ecc89bdc18c009a521295773cd33613f17b2c
js <js@js>
2024-06-16 12:09:01 +0200

    adding alergens

---
+++ src/api/alergen/controllers/alergen.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alergen controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::alergen.alergen');
---
+++ src/api/alergen/routes/alergen.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alergen router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::alergen.alergen');
